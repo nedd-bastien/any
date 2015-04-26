ec24937038923dc7d2d2866814f31bda872840da
yester30 <bastien.asf@gmail.com>
2015-04-26 15:59:19 +0200

    Commit depuis vieilles sources pleines d'erreurs

---
+++ Constants.js
@@ -0,0 +1,7 @@
+public class Constants()
+{
+	
+	const STATUS_PREP = 1;
+	const STATUS_LIVE = 2;
+	const STATUS_DEAD = 3; // ou detruit, si item
+}
---
+++ Element.js
@@ -0,0 +1,10 @@
+public class Element()
+{
+	
+	var id;
+	var sentient;
+	var HP;
+	var status = Constants.STATUS_PREP; //constant
+	
+}
+
